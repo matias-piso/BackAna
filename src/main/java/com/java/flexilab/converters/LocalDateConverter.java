151fccad5f426f6a9ccc842dd9227f3114a3c4cb
Matias Piso <mpiso@frba.utn.edu.ar>
2023-02-16 00:04:41 -0300

    converter localdatetime y entitimanagerhelper creados

---
+++ src/main/java/com/java/flexilab/converters/LocalDateConverter.java
@@ -0,0 +1,25 @@
+package capgemini.tp.campgemini20.converters;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Converter (autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    //convierte desde objetos tipo java a objetos tipo sql
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        //si es null, devuelve null sino devuelve el objeto convertido
+        return (localDate == null ? null : Date.valueOf(localDate));
+    }
+
+    //convierte desde la base de datos a objetos java
+    @Override
+    public LocalDate convertToEntityAttribute(Date date) {
+        //si es null devuelve null sino devuelve el date
+        return (date == null ? null : date.toLocalDate());
+    }
+
+}
