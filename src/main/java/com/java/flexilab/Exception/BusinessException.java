ccdb795ee4d79ba4d02fda4d56a3b81e5c28b865
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-08-11 12:21:06 -0300

    se arregle error en 'errores globales', falta implementar y testear el funcionamiento de los errores globales

--- src/main/java/com/java/flexilab/Exception/BusinessException.java
+++ src/main/java/com/java/flexilab/Exception/BusinessException.java
@@ -1,2 +1,15 @@
-package com.java.flexilab.Exception;public class BussinesException {
+package com.java.flexilab.Exception;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+public class BusinessException extends RuntimeException {
+    private HttpStatus httpStatus;
+
+    public BusinessException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
 }
--- src/main/java/com/java/flexilab/Exception/ControllerAdvice.java
+++ src/main/java/com/java/flexilab/Exception/ControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.java.flexilab.controllers;
 
 import com.java.flexilab.DTO.ErrorDTO;
+import com.java.flexilab.Exception.BusinessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
